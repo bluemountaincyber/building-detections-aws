dc1257cae68319a8bb688cea9fe8bd2bed986148
ryananicholson <ryananicholson@gmail.com>
2023-03-17 10:56:34 -0400

    fix: pushd command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pushd >/dev/null
+pushd ~/building-detections-aws >/dev/null
 aws cloudformation deploy --stack-name building-detections --template-file ../BuildingDetections.yaml --capabilities CAPABILITY_IAM
 popd >/dev/null
