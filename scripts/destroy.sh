03c0fd395d62a300289e606d8b21cdb05a4e6d45
ryananicholson <ryananicholson@gmail.com>
2023-03-18 10:23:15 -0400

    fix: More cleanup

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -2,4 +2,6 @@
 
 BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | startswith("cloudlogs-")) | .Name')
 aws s3 rm s3://$BUCKET --recursive
+aws s3api delete-bucket --bucket $BUCKET
+aws cloudtrail delete-trail --name security
 aws cloudformation delete-stack --stack-name building-detections
